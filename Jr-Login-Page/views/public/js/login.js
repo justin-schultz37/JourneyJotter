c4fb359e07fa6e9de734d67e38795818f5c3039f
Hsolojr <139496108+Hsolojr@users.noreply.github.com>
2023-11-02 09:16:27 -0700

    Added Login Credentials for existing Users

--- Jr-Login-Page/views/public/js/login.js
+++ Jr-Login-Page/views/public/js/login.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function () {
       const emailInput = document.getElementById("email-login").value;
       const passwordInput = document.getElementById("password-login").value;
   
-      // Replace this with your authentication logic
+      // Replace this with authentication 
       if (validateLogin(emailInput, passwordInput)) {
         // Redirect to another page
         window.location.href = "/";
@@ -17,21 +17,21 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   
     function validateLogin(email, password) {
-      // Replace this with actual authentication logic
+      // Replace this with actual authentication 
       const users = [
         { email: "user1@example.com", password: "password1" },
         { email: "user2@example.com", password: "password2" },
-        // Add more user objects as needed
+        // Add more user objects 
       ];
   
       // Check if the provided email and password match any user in the array
       for (const user of users) {
         if (user.email === email && user.password === password) {
-          return true; // Authentication successful
+          return true; // successful
         }
       }
   
-      return false; // Authentication failed
+      return false; // failed
     }
   });
   
