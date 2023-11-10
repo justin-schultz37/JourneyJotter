050f2ddb0d348bf6579fc39953e16d45675a5b9e
Hsolojr <139496108+Hsolojr@users.noreply.github.com>
2023-11-09 21:47:53 -0800

    ReadME added to file

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ const sequelize = new Sequelize(
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-        host: process.env.local_host,
+        host: '127.0.0.1',
         dialect: 'mysql',
         port: 3306,
     }
